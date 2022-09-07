65ad2290f1d84fcf52db6e42a97a45952ffb9e69
adhamsamir <74482632+adhamsamir@users.noreply.github.com>
2022-09-07 13:36:05 +0200

    Update app.js
    Created a function to link the items in the navbar to sections

--- js/app.js
+++ js/app.js
@@ -1,62 +1,13 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+const navlist=document.getElementById("navbar__list");
+const sections=Array.from(document.querySelectorAll("section"));
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
+function createListItem(){
+    for (sec of sections){
+        listItem=document.createElement("li");
+        
+        listItem.innerHtml=`<li> <a href = " # ${sec.id}" data-nav= " # ${sec.id}" class= "menu__link"> ${sec.dataset.nav}</a></li>`
+        navlist.appendChild(listItem);
+    }
+}
 
-/**
- * Define Global Variables
- * 
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+createListItem();
