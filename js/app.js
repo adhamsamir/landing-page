7b7c26de12ea4ea9876fda9470bd8f62c1390fbd
adhamsamir <74482632+adhamsamir@users.noreply.github.com>
2022-09-07 14:14:19 +0200

    Update app.js
    function to scroll to section when navbar is clickes

--- js/app.js
+++ js/app.js
@@ -50,3 +50,13 @@ function addActiveClass(section) {
   }
  
   document.addEventListener('scroll', makeActiveSection);
+  //Scroll to section when Navbar link is clicked
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior : "smooth"
+        });
+    });
+});
